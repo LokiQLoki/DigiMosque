0788dc744de69c2b721348c47b09f7945289b620
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2019-08-28 03:24:37 +0530

    images

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,send_from_directory
 
 application = Flask(__name__)
 
@@ -21,7 +21,7 @@ import pickle
 
 # setup_app(application)
 
-
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @application.route("/")
 def hello():
@@ -49,16 +49,30 @@ def search_in_quran():
     print(mosque_name,mosque_lat,mosque_lon,fajr_time,zuhur_time,asar_time,maghrib_time,isha_time,contact_num)
 
 
+    #now to store the images
+    folder_name=mosque_name+"_"+contact_num
+    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            render_template("Error.html", message="Files uploaded are not supported...")
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
 
 
 
-    
-    
-            
 
-    
-
- 
     return "cool"
 
 
